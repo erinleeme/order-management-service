643c98355445f081529a7039e61c42772a90c57d
erinleeme <gusdk1793@gmail.com>
2024-11-27 09:51:37 +0900

    project init

---
+++ src/main/java/order/management/ex/ExApplication.java
@@ -0,0 +1,13 @@
+package order.management.ex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExApplication.class, args);
+	}
+
+}
